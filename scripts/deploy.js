626a8428db832db9c89717a8ea200e2d423ec9a5
3ddelano <3ddelano@gmail.com>
2022-05-28 21:36:26 +0530

    update deploy script

--- scripts/deploy.js
+++ scripts/deploy.js
@@ -1,56 +1,142 @@
+const { parseEther, parseUnits } = ethers.utils;
+
+let testData = {
+    baseFee: parseEther("0.11"),
+    percentFee: 75,
+    managers: ["0x70997970C51812dc3A010C7d01b50e0d17dc79C8"],
+    // Initial 5 items
+    itemNames: [
+        "Chocolate Cake",
+        "Bread Loaf",
+        "Vanilla Cupcake",
+        "Strawberry Muffin",
+        "Garlic Bread",
+    ],
+    itemPrices: [
+        parseUnits("2"),
+        parseUnits("0.67"),
+        parseUnits("0.559"),
+        parseUnits("0.671"),
+        parseUnits("0.223"),
+    ],
+    itemStocks: [-1, 200, 150, 150, 300],
+    itemTastes: ["sweet", "starchy", "sweet", "sweet", "pungent"],
+    itemColors: ["brown", "white", "off-white", "red", "white"],
+    itemTextures: ["silky", "soft", "soft", "soft", "soft"],
+    itemSizes: ["large", "medium", "small", "small", "small"],
+
+    // Initial 5 pickupLocations
+    pickLocNames: [
+        "Pastry Palacio",
+        "FZ Cakes",
+        "Monginia",
+        "Pastry Factory",
+        "Yummy Eats",
+    ],
+    pickLocLocations: [
+        "Shop No 10, Tps Vi, Milan Subway, Rizvi Nagar, Sv Road, Near Dhiraj Heritege, Santacruz (west)",
+        "13, 3rd Flr, Heena Shopping Arcade, S V Rd, Below Allahbad Bank, Jogeshwari (west)",
+        "214a, Fakeer M., Jain Mohammed Chawl, Opp Old Post Office, Dharavi",
+        "Da-3/1, Vikas Marg Extn, Shakarpur",
+        "Gate No 7, Opp.mhb Colony, Kharodi",
+    ],
+    pickLocCities: ["Mumbai", "Mumbai", "Mumbai", "Delhi", "Mumbai"],
+    pickLocStates: [
+        "Maharastra",
+        "Maharastra",
+        "Maharastra",
+        "Delhi",
+        "Maharastra",
+    ],
+    pickLocPhones: [
+        "+919202727371",
+        "+917112535346",
+        "+917222768894",
+        "+919222642190",
+        "+919402457009",
+    ],
+};
+
 async function main() {
-  // This is just a convenience check
-  if (network.name === "hardhat") {
-    console.warn(
-      "You are trying to deploy a contract to the Hardhat Network, which" +
-        "gets automatically created and destroyed every time. Use the Hardhat" +
-        " option '--network localhost'"
+    // This is just a convenience check
+    if (network.name === "hardhat") {
+        console.warn(
+            "You are trying to deploy a contract to the Hardhat Network, which" +
+                "gets automatically created and destroyed every time. Use the Hardhat" +
+                " option '--network localhost'"
+        );
+    }
+
+    // ethers is available in the global scope
+    const [deployer] = await ethers.getSigners();
+    console.log(
+        "Deploying the contracts with the account:",
+        await deployer.getAddress()
     );
-  }
 
-  // ethers is available in the global scope
-  const [deployer] = await ethers.getSigners();
-  console.log(
-    "Deploying the contracts with the account:",
-    await deployer.getAddress()
-  );
+    console.log("Account balance:", (await deployer.getBalance()).toString());
 
-  console.log("Account balance:", (await deployer.getBalance()).toString());
+    const ShopAgileWeb3 = await ethers.getContractFactory("ShopAgileWeb3");
+    const shopAgileWeb3 = await ShopAgileWeb3.deploy(
+        testData.baseFee,
+        testData.percentFee,
+        testData.itemPrices,
+        testData.itemStocks,
+        testData.itemNames,
+        testData.pickLocNames,
+        testData.pickLocLocations,
+        testData.pickLocCities,
+        testData.pickLocStates,
+        testData.pickLocPhones
+    );
+    await shopAgileWeb3.deployed();
 
-  const ShopAgileWeb3 = await ethers.getContractFactory("ShopAgileWeb3");
-  const shopAgileWeb3 = await ShopAgileWeb3.deploy();
-  await shopAgileWeb3.deployed();
+    let itemsToUpdate = [];
 
-  console.log("ShopAgileWeb3 address:", shopAgileWeb3.address);
+    for (let i = 0; i < testData.itemTastes.length; ++i) {
+        itemsToUpdate.push(
+            shopAgileWeb3.updateItemAttributes(
+                i,
+                testData.itemTastes[i],
+                testData.itemColors[i],
+                testData.itemTextures[i],
+                testData.itemSizes[i]
+            )
+        );
+    }
 
-  // We also save the contract's artifacts and address in the frontend directory
-  saveFrontendFiles(shopAgileWeb3);
+    await Promise.all(itemsToUpdate);
+
+    console.log("ShopAgileWeb3 address:", shopAgileWeb3.address);
+
+    // We also save the contract's artifacts and address in the frontend directory
+    saveFrontendFiles(shopAgileWeb3);
 }
 
 function saveFrontendFiles(shopAgileWeb3) {
-  const fs = require("fs");
-  const contractsDir = __dirname + "/../frontend/src/contracts";
+    const fs = require("fs");
+    const contractsDir = __dirname + "/../frontend/src/contracts";
 
-  if (!fs.existsSync(contractsDir)) {
-    fs.mkdirSync(contractsDir);
-  }
+    if (!fs.existsSync(contractsDir)) {
+        fs.mkdirSync(contractsDir);
+    }
 
-  fs.writeFileSync(
-    contractsDir + "/contract-address.json",
-    JSON.stringify({ ShopAgileWeb3: shopAgileWeb3.address }, undefined, 2)
-  );
+    fs.writeFileSync(
+        contractsDir + "/contract-address.json",
+        JSON.stringify({ ShopAgileWeb3: shopAgileWeb3.address }, undefined, 2)
+    );
 
-  const TokenArtifact = artifacts.readArtifactSync("ShopAgileWeb3");
+    const TokenArtifact = artifacts.readArtifactSync("ShopAgileWeb3");
 
-  fs.writeFileSync(
-    contractsDir + "/ShopAgileWeb3.json",
-    JSON.stringify(TokenArtifact, null, 2)
-  );
+    fs.writeFileSync(
+        contractsDir + "/ShopAgileWeb3.json",
+        JSON.stringify(TokenArtifact, null, 2)
+    );
 }
 
 main()
-  .then(() => process.exit(0))
-  .catch((error) => {
-    console.error(error);
-    process.exit(1);
-  });
+    .then(() => process.exit(0))
+    .catch((error) => {
+        console.error(error);
+        process.exit(1);
+    });
